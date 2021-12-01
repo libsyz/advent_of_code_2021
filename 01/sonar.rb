88e19a762f8b873db00c936d14b607eae72d058c
Miguel Jimenez <denkatalabeno@gmail.com>
2021-12-01 13:54:23 +0800

    passing AoC tests for part two of the exercise

--- 01/sonar.rb
+++ 01/sonar.rb
@@ -1,11 +1,34 @@
-require 'open-uri'
 require 'pry-byebug'
-
-file = open('https://adventofcode.com/2021/day/1/input')
-
-
+measurements = []
+File.open('./input.txt').each do |line|
+  measurements << line.chomp.to_i
+end
 
 
 def sonar(measurements)
+  previous = nil
+  increases = 0
+  measurements.each do |num|
+    if previous && previous < num
+      increases += 1
+    end
+    previous = num
+  end
+  increases
+end
 
+# Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum.
+# So, compare A with B, then compare B with C, then C with D, and so on.
+# Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+
+
+def sonar_sliding(measurements)
+
+  sums = measurements.map.with_index do |el, index|
+    if measurements[ + 1] && measurements[index + 2]
+      el + measurements[index + 1] + measurements[index + 2]
+    end
+  end.compact
+  sonar(sums)
 end
--- 01/spec/sonar_spec.rb
+++ 01/spec/sonar_spec.rb
@@ -2,9 +2,55 @@ require './../sonar'
 require 'rspec'
 
 describe '#sonar' do
-  it '#counts the number of times the depth increases' do
-    measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    counts = sonar(measurements)
-    expect(counts).to eq(7)
+  context "when the measurements are always different from each other" do
+    it '#correctly counts the number of times the depth increases' do
+      measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+      counts = sonar(measurements)
+      expect(counts).to eq(7)
+    end
+  end
+
+  context "when the measurements always increase " do
+    it '#correctly counts the number of times the depth increases' do
+      measurements = [199, 200, 201, 202, 203, 204, 205, 206, 207, 208]
+      counts = sonar(measurements)
+      expect(counts).to eq(measurements.length - 1)
+    end
+  end
+
+  context "when the measurements always decrease" do
+    it '#tallies the counts of depth increases to 0' do
+      measurements = [208, 207, 206, 205, 204, 203, 202, 201, 200, 199]
+      counts = sonar(measurements)
+      expect(counts).to eq(0)
+    end
+  end
+
+  context "when the measurements sometimes stay the same" do
+    it '#correctly counts the amount of depth increases' do
+      measurements = [100, 100, 100, 101, 100]
+      counts = sonar(measurements)
+      expect(counts).to eq(1)
+    end
+  end
+end
+
+describe "#sonar_sliding" do
+  context "when the measurements are always different from each other" do
+    it '#correctly counts the number of times the depth increases' do
+      measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+#                     A   A    A    607
+#                         B    B    B    618
+#                              C    C    C    618
+#                                   D    D    D    647
+#                                        E    E    E   716
+#                                             F    F   F  769
+#                                                  G   G  G  792
+#                                                      H  H  H
+#                                                            Stop
+
+      counts = sonar_sliding(measurements)
+      expect(counts).to eq(5)
+    end
   end
 end
