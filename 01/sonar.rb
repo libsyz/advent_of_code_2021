a102371988d7fd70595b41379a01455924042fc7
Miguel Jimenez <denkatalabeno@gmail.com>
2021-12-01 14:37:37 +0800

    refactored sonar method

--- 01/sonar.rb
+++ 01/sonar.rb
@@ -1,30 +1,19 @@
 require 'pry-byebug'
-measurements = []
-File.open('./input.txt').each do |line|
-  measurements << line.chomp.to_i
-end
+# measurements = []
+# File.open('./input.txt').each do |line|
+#   measurements << line.chomp.to_i
+# end
 
 
-def sonar(measurements)
-  previous = nil
-  increases = 0
-  measurements.each do |num|
-    if previous && previous < num
-      increases += 1
-    end
-    previous = num
+def sonar(measures)
+  measures.each_with_index.reduce(0) do |acc, (_, idx)|
+    measures[idx] > measures[idx-1] && idx != 0 ? acc += 1 : acc
   end
-  increases
 end
-
-# Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum.
-# So, compare A with B, then compare B with C, then C with D, and so on.
-# Stop when there aren't enough measurements left to create a new three-measurement sum.
 
 
 
 def sonar_sliding(measurements)
-
   sums = measurements.map.with_index do |el, index|
     if measurements[index + 1] && measurements[index + 2]
       el + measurements[index + 1] + measurements[index + 2]
@@ -33,4 +22,4 @@ def sonar_sliding(measurements)
   sonar(sums)
 end
 
-p sonar_sliding(measurements)
+# p sonar_sliding(measurements)
