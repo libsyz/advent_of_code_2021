d7f60d97c1013418a3b7f6e1540dc61527950383
Miguel Jimenez <denkatalabeno@gmail.com>
2021-12-02 15:17:38 +0800

    not sure why the res is not moving at all

--- 02/position.rb
+++ 02/position.rb
@@ -1,4 +1,10 @@
-
+# require 'pry-byebug'
+# commands = []
+# File.open('./input.txt').each do |line|
+#   # binding.pry
+#   command, num = line.chomp.split
+#   commands << [command.to_sym, num.to_i]
+# end
 
 def position(commands)
   pos = { horizontal: 0, vertical: 0}
@@ -16,3 +22,23 @@ def position(commands)
   end
   pos
 end
+
+
+def position_with_aim(commands)
+  pos = { horizontal: 0, vertical: 0, aim: 0}
+  commands.each do |cmd|
+    case cmd[0]
+    when :up
+      pos[:aim] -= cmd[1]
+    when :down
+      pos[:aim] += cmd[1]
+    when :forward
+      pos[:horizontal] += cmd[1]
+      pos[:vertical] += (pos[:aim] * cmd[1])
+    else
+      # no op
+    end
+  end
+  pos
+end
+# p position(commands)
--- 02/spec/position_spec.rb
+++ 02/spec/position_spec.rb
@@ -20,3 +20,23 @@ describe "#position" do
     end
   end
 end
+
+
+describe "#position_with_aim" do
+  context "with data from AoC example" do
+    commands = [
+                [:forward, 5],
+                [:down, 5],
+                [:forward, 8],
+                [:up, 3],
+                [:down, 8],
+                [:forward, 2]
+              ]
+    it "calculates the right total positions" do
+      res = position(commands)
+       expect(res[:horizontal]).to eq(15)
+       expect(res[:vertical]).to eq(60)
+       expect(res[:horizontal] * res[:vertical]).to eq(900)
+    end
+  end
+end
