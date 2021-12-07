594617a7c43b01f09f3f7417b0abed4850696d5e
Miguel Jimenez <denkatalabeno@gmail.com>
2021-12-07 16:24:04 +0800

    problem solved! second solution needs to be optimized

--- 07/spec/crab_spec.rb
+++ 07/spec/crab_spec.rb
@@ -4,11 +4,19 @@ require 'rspec'
 
 
 describe "#crab" do
-  context "part 1" do
+  context "part 1 - crabs consume 1 fuel per movement" do
     crabs = [16,1,2,0,4,2,7,1,2,14]
     it 'calculates the minimum amount fuel possible to align all crabs in the same position' do
       res = align(crabs)
-      expect(res).to eq(14)
+      expect(res).to eq(37)
+    end
+  end
+
+  context "part 2 - crabs consume +1 incremental fuel on every movement" do
+    crabs = [16,1,2,0,4,2,7,1,2,14]
+    it 'calculates the minimum amount fuel possible to align all crabs in the same position' do
+      res = align_incremental(crabs)
+      expect(res).to eq(168)
     end
   end
 end
