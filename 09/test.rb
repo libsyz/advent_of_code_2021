3cc806ee3b5908a1e091b95a9cd93ae5b33559d9
Miguel Jimenez <denkatalabeno@gmail.com>
2021-12-10 09:55:26 +0800

    prototype seems to work with small examples!

--- 09/test.rb
+++ 09/test.rb
@@ -1,34 +1,40 @@
 
 require 'pry-byebug'
 
-def get_cave(arr, start_point)
-  queue = [start_point]
+def get_cave(arr, starting_point)
+  queue = [starting_point]
   already_processed = []
   valid = []
 
   while !queue.empty?
-    queue.each do |coord|
-      valid << coord if arr[coord] != 9 || !already_processed.include?(coord)
-      already_processed << coord
-      adjacents = get_valid_adjacents(arr, coord)
-      queue.delete(coord)
 
-      adjacents.each do |coord|
-        queue << coord unless already_processed.include?(coord)
+    queue.each do |point|
+      valid << point if arr[point[0]][point[1]] != 9 || !already_processed.include?(point)
+      already_processed << point
+      adjacents = get_valid_adjacents(arr, point)
+      queue.delete(point)
+
+      adjacents.each do |point|
+        queue << point unless already_processed.include?(point)
       end
     end
   end
 
-  valid.map { |idx| arr[idx] }
+  valid.map { |point| arr[point[0]][point[1]]   }
 
 end
 
 
-def get_valid_adjacents(arr, index)
+def get_valid_adjacents(arr, point)
+  row, pos = point
   # because that would mean there's no valid coords
-  left = arr[index - 1].zero? || arr[index - 1] == 9 ? nil : index - 1
-  right = arr[index + 1].nil? || arr[index + 1] == 9 ? nil : index + 1
-  [left, right].compact
+  behind = pos.zero? || arr[row][pos - 1] == 9 ? nil : [row, pos - 1]
+  ahead = arr[row][pos + 1].nil? || arr[row][pos + 1] == 9 ? nil : [row, pos + 1]
+  above = row.zero? || arr[row - 1][pos] == 9 ? nil : [row - 1, pos]
+  below = arr[row + 1].nil? || arr[row + 1][pos] == 9 ? nil : [row + 1, pos]
+  [behind, ahead, above, below].compact
 end
 
-p get_cave([1, 1, 9, 1, 9, 1, 1], 3)
+p get_cave([[9, 9, 9, 2, 9, 9, 9],
+            [9, 9, 2, 1, 2, 9, 9],
+            [9, 9, 9, 2, 9, 9, 9]], [1, 3])
