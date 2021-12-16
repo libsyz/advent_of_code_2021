622c0b3e1716a7c1e13ec138296a76fa8b5d71f6
Miguel Jimenez <denkatalabeno@gmail.com>
2021-12-17 01:11:16 +0800

    got it working faaaaast

--- 14/polymer.rb
+++ 14/polymer.rb
@@ -1,149 +1,173 @@
 require 'pry-byebug'
 
-# KEY = {
-#   'CH' => 'B',
-#   'HH' => 'N',
-#   'CB' => 'H',
-#   'NH' => 'C',
-#   'HB' => 'C',
-#   'HC' => 'B',
-#   'HN' => 'C',
-#   'NN' => 'C',
-#   'BH' => 'H',
-#   'NC' => 'B',
-#   'NB' => 'B',
-#   'BN' => 'B',
-#   'BB' => 'N',
-#   'BC' => 'B',
-#   'CC' => 'N',
-#   'CN' => 'C'
-# }
-
-KEY = {
-  'HH' => 'K',
-  'PS' => 'P',
-  'BV' => 'H',
-  'HB' => 'H',
-  'CK' => 'F',
-  'FN' => 'B',
-  'PV' => 'S',
-  'KK' => 'F',
-  'OF' => 'C',
-  'SF' => 'B',
-  'KB' => 'S',
-  'HO' => 'O',
-  'NH' => 'N',
-  'ON' => 'V',
-  'VF' => 'K',
-  'VP' => 'K',
-  'PH' => 'K',
-  'FF' => 'V',
-  'OV' => 'N',
-  'BO' => 'K',
-  'PO' => 'S',
-  'CH' => 'N',
-  'FO' => 'V',
-  'FB' => 'H',
-  'FV' => 'N',
-  'FK' => 'S',
-  'VC' => 'V',
-  'CP' => 'K',
-  'CO' => 'K',
-  'SV' => 'N',
-  'PP' => 'B',
-  'BS' => 'P',
-  'VS' => 'C',
-  'HV' => 'H',
-  'NN' => 'F',
-  'NK' => 'C',
-  'PC' => 'V',
-  'HS' => 'S',
-  'FS' => 'S',
-  'OB' => 'S',
-  'VV' => 'N',
-  'VO' => 'P',
-  'KV' => 'F',
-  'SK' => 'O',
-  'BC' => 'P',
-  'BP' => 'F',
-  'NS' => 'P',
-  'SN' => 'S',
-  'NC' => 'N',
-  'FC' => 'V',
-  'CN' => 'S',
-  'OK' => 'B',
-  'SC' => 'N',
-  'HN' => 'B',
-  'HP' => 'B',
-  'KP' => 'B',
-  'CB' => 'K',
-  'KF' => 'C',
-  'OS' => 'B',
-  'BH' => 'O',
-  'PN' => 'K',
-  'VN' => 'O',
-  'KH' => 'F',
-  'BF' => 'H',
-  'HF' => 'K',
-  'HC' => 'P',
-  'NP' => 'H',
-  'KO' => 'K',
-  'CF' => 'H',
-  'BK' => 'O',
-  'OH' => 'P',
-  'SO' => 'F',
-  'BB' => 'F',
-  'VB' => 'K',
-  'SP' => 'O',
-  'SH' => 'O',
-  'PK' => 'O',
-  'HK' => 'P',
-  'CC' => 'V',
-  'NB' => 'O',
-  'NV' => 'F',
-  'OO' => 'F',
-  'VK' => 'V',
-  'FH' => 'H',
-  'SS' => 'C',
-  'NO' => 'P',
-  'CS' => 'H',
-  'BN' => 'V',
-  'FP' => 'N',
-  'OP' => 'N',
-  'PB' => 'P',
-  'OC' => 'O',
-  'SB' => 'V',
-  'VH' => 'O',
-  'KS' => 'B',
-  'PF' => 'N',
-  'KN' => 'H',
-  'NF' => 'N',
-  'CV' => 'K',
-  'KC' => 'B',
+REF = {
+  'CH' => 'B',
+  'HH' => 'N',
+  'CB' => 'H',
+  'NH' => 'C',
+  'HB' => 'C',
+  'HC' => 'B',
+  'HN' => 'C',
+  'NN' => 'C',
+  'BH' => 'H',
+  'NC' => 'B',
+  'NB' => 'B',
+  'BN' => 'B',
+  'BB' => 'N',
+  'BC' => 'B',
+  'CC' => 'N',
+  'CN' => 'C'
 }
 
+# REF = {
+#   'HH' => 'K',
+#   'PS' => 'P',
+#   'BV' => 'H',
+#   'HB' => 'H',
+#   'CK' => 'F',
+#   'FN' => 'B',
+#   'PV' => 'S',
+#   'KK' => 'F',
+#   'OF' => 'C',
+#   'SF' => 'B',
+#   'KB' => 'S',
+#   'HO' => 'O',
+#   'NH' => 'N',
+#   'ON' => 'V',
+#   'VF' => 'K',
+#   'VP' => 'K',
+#   'PH' => 'K',
+#   'FF' => 'V',
+#   'OV' => 'N',
+#   'BO' => 'K',
+#   'PO' => 'S',
+#   'CH' => 'N',
+#   'FO' => 'V',
+#   'FB' => 'H',
+#   'FV' => 'N',
+#   'FK' => 'S',
+#   'VC' => 'V',
+#   'CP' => 'K',
+#   'CO' => 'K',
+#   'SV' => 'N',
+#   'PP' => 'B',
+#   'BS' => 'P',
+#   'VS' => 'C',
+#   'HV' => 'H',
+#   'NN' => 'F',
+#   'NK' => 'C',
+#   'PC' => 'V',
+#   'HS' => 'S',
+#   'FS' => 'S',
+#   'OB' => 'S',
+#   'VV' => 'N',
+#   'VO' => 'P',
+#   'KV' => 'F',
+#   'SK' => 'O',
+#   'BC' => 'P',
+#   'BP' => 'F',
+#   'NS' => 'P',
+#   'SN' => 'S',
+#   'NC' => 'N',
+#   'FC' => 'V',
+#   'CN' => 'S',
+#   'OK' => 'B',
+#   'SC' => 'N',
+#   'HN' => 'B',
+#   'HP' => 'B',
+#   'KP' => 'B',
+#   'CB' => 'K',
+#   'KF' => 'C',
+#   'OS' => 'B',
+#   'BH' => 'O',
+#   'PN' => 'K',
+#   'VN' => 'O',
+#   'KH' => 'F',
+#   'BF' => 'H',
+#   'HF' => 'K',
+#   'HC' => 'P',
+#   'NP' => 'H',
+#   'KO' => 'K',
+#   'CF' => 'H',
+#   'BK' => 'O',
+#   'OH' => 'P',
+#   'SO' => 'F',
+#   'BB' => 'F',
+#   'VB' => 'K',
+#   'SP' => 'O',
+#   'SH' => 'O',
+#   'PK' => 'O',
+#   'HK' => 'P',
+#   'CC' => 'V',
+#   'NB' => 'O',
+#   'NV' => 'F',
+#   'OO' => 'F',
+#   'VK' => 'V',
+#   'FH' => 'H',
+#   'SS' => 'C',
+#   'NO' => 'P',
+#   'CS' => 'H',
+#   'BN' => 'V',
+#   'FP' => 'N',
+#   'OP' => 'N',
+#   'PB' => 'P',
+#   'OC' => 'O',
+#   'SB' => 'V',
+#   'VH' => 'O',
+#   'KS' => 'B',
+#   'PF' => 'N',
+#   'KN' => 'H',
+#   'NF' => 'N',
+#   'CV' => 'K',
+#   'KC' => 'B',
+# }
+
+# def polymerize!(string, steps: 1)
+
+#   steps.times do |step|
+#    string = string.split('').each_cons(2).map.with_index do |pair, index|
+#       new_element = KEY[pair.join]
+#       index.zero? ? "#{pair.first}#{new_element}#{pair.last}" : "#{new_element}#{pair.last}"
+#     end.join
+#   end
+#   return string
+# end
+
+# puts polymerize!('SHHNCOPHONHFBVNKCFFC', steps: 40)
+
 def polymerize!(string, steps: 1)
-  steps.times do |step|
-    p "on step #{step}"
-   string = string.split('').each_cons(2).map.with_index do |pair, index|
-      new_element = KEY[pair.join]
-      index.zero? ? "#{pair.first}#{new_element}#{pair.last}" : "#{new_element}#{pair.last}"
-    end.join
+  start_state = string.split('').each_cons(2).map(&:join)
+  letters_hash = Hash.new(0)
+  state_hash = Hash.new(0)
+
+  REF.keys.each do |key|
+    state_hash[key] = 0
   end
-  return string
+
+
+  start_state.each do |pair|
+    state_hash[pair] += 1
+  end
+
+  letters_hash.merge!(string.split('').tally)
+  steps.times do |step|
+    step_state = Hash.new(0)
+    state_hash.keys.select { |key| state_hash[key] > 0}.each do |key|
+
+        new_letter = REF[key]
+        new_key_behind = key[0] + new_letter
+        new_key_after = new_letter + key[1]
+
+        increase = state_hash[key]
+        letters_hash[new_letter] += increase
+        step_state[new_key_behind] += increase
+        step_state[new_key_after] += increase
+        step_state[key] -= increase
+    end
+    step_state.keys.each do |key|
+      state_hash[key] += step_state[key]
+    end
+  end
+
+  letters_hash
 end
-
-
-res = polymerize!('SHHNCOPHONHFBVNKCFFC', steps:40)
-
-elements = KEY.values.join.split('').uniq
-
-counts = []
-
-elements.each do |el|
-  counts << res.count(el)
-end
-
-ordered = counts.sort
-
-puts ordered.last - ordered.first
--- 14/spec/polymer_spec.rb
+++ 14/spec/polymer_spec.rb
@@ -4,38 +4,48 @@ require '../polymer.rb'
 describe "#polymerize!" do
   context "with a starting polymer" do
     let(:polymer) { 'NNCB' }
-    it "returns the right polymer after 1 step" do
+    xit "returns the right polymer after 1 step" do
       res = polymerize!(polymer)
       expect(res).to eq('NCNBCHB')
     end
 
-    it "returns the right polymer after 2 steps" do
+    xit "returns the right polymer after 2 steps" do
       res = polymerize!(polymer, steps: 2)
       expect(res).to eq('NBCCNBBBCBHCB')
     end
 
-    it "returns the right polymer after 3 steps" do
+    xit "returns the right polymer after 3 steps" do
       res = polymerize!(polymer, steps: 3)
       expect(res).to eq('NBBBCNCCNBBNBNBBCHBHHBCHB')
     end
 
-    it "returns the right polymer after 4 steps" do
+    xit "returns the right polymer after 4 steps" do
       res = polymerize!(polymer, steps: 4)
       expect(res).to eq('NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB')
     end
 
+    it "returns the right polymer length after 1 step" do
+      res = polymerize!(polymer)
+      expect(res.values.sum).to eq(7)
+    end
+
+    xit "returns the right polymer after 2 steps" do
+      res = polymerize!(polymer, steps: 2)
+      expect(res.length).to eq(13)
+    end
+
     it "returns a polymer with the right length after 5 steps" do
       res = polymerize!(polymer, steps: 5)
-      expect(res.length).to eq(97)
+      expect(res.values.sum).to eq(97)
     end
 
     it "returns a polymer with the right component count after 10 steps" do
       # After step 10, B occurs 1749 times, C occurs 298 times, H occurs 161 times, and N occurs 865 times
       res = polymerize!(polymer, steps: 10)
-      expect(res.count('B')).to eq(1749)
-      expect(res.count('C')).to eq(298)
-      expect(res.count('H')).to eq(161)
-      expect(res.count('N')).to eq(865)
+      expect(res['B']).to eq(1749)
+      expect(res['C']).to eq(298)
+      expect(res['H']).to eq(161)
+      expect(res['N']).to eq(865)
     end
 
 
