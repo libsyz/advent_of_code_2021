848d2c38d84531d22e0fca56dc0cfcfdf0caeaad
Miguel Jimenez <denkatalabeno@gmail.com>
2021-12-25 01:25:04 +0800

    passing first star of the challenge, go bfs!

--- 12/route.rb
+++ 12/route.rb
@@ -14,6 +14,10 @@ class Node
     "#{@name} -> #{links.map(&:name)}"
   end
 
+  def single?
+    @links.size == 1 && @links.last.name == @links.last.name.downcase
+  end
+
   def to_s
     "#{@name}"
   end
@@ -51,48 +55,48 @@ end
 #         'kj-HN',
 #         'kj-dc']
 
-list = ['fs-end',
-        'he-DX',
-        'fs-he',
-        'start-DX',
-        'pj-DX',
-        'end-zg',
-        'zg-sl',
-        'zg-pj',
-        'pj-he',
-        'RW-he',
-        'fs-DX',
-        'pj-RW',
-        'zg-RW',
-        'start-pj',
-        'he-WI',
-        'zg-he',
-        'pj-fs',
-        'start-RW']
+# list = ['fs-end',
+#         'he-DX',
+#         'fs-he',
+#         'start-DX',
+#         'pj-DX',
+#         'end-zg',
+#         'zg-sl',
+#         'zg-pj',
+#         'pj-he',
+#         'RW-he',
+#         'fs-DX',
+#         'pj-RW',
+#         'zg-RW',
+#         'start-pj',
+#         'he-WI',
+#         'zg-he',
+#         'pj-fs',
+#         'start-RW']
 
-# puzzle_input =  ['nu-start',
-#                 'rt-start',
-#                 'db-qh',
-#                 'PE-end',
-#                 'sl-rt',
-#                 'qh-end',
-#                 'ZH-rt',
-#                 'nu-rt',
-#                 'PE-db',
-#                 'db-sl',
-#                 'nu-ZH',
-#                 'nu-qh',
-#                 'PE-qh',
-#                 'ZH-db',
-#                 'ne-end',
-#                 'ne-ZH',
-#                 'QG-db',
-#                 'qh-sl',
-#                 'ZH-qh',
-#                 'start-ZH',
-#                 'nu-PE',
-#                 'uf-db',
-#                 'ne-sl']
+puzzle_input =  ['nu-start',
+                'rx-start',
+                'db-qh',
+                'PE-end',
+                'sl-rx',
+                'qh-end',
+                'ZH-rx',
+                'nu-rx',
+                'PE-db',
+                'db-sl',
+                'nu-ZH',
+                'nu-qh',
+                'PE-qh',
+                'ZH-db',
+                'ne-end',
+                'ne-ZH',
+                'QG-db',
+                'qh-sl',
+                'ZH-qh',
+                'start-ZH',
+                'nu-PE',
+                'uf-db',
+                'ne-sl']
 
 class NodeParser
   attr_reader :nodes
@@ -120,9 +124,9 @@ class NodeParser
 end
 
 
-node_list = NodeParser.new.generate(puzzle_input).nodes
+# node_list = NodeParser.new.generate(puzzle_input).nodes
 
-start_node = node_list.find { |nd| nd.name == 'start' }
+# start_node = node_list.find { |nd| nd.name == 'start' }
 
 
 
@@ -157,26 +161,46 @@ def get_routes(start, routes)
   return routes
 end
 
+def get_long_routes(start, routes)
+  queue = [ [start,'start'] ]
 
-puts get_routes(start_node, [])
+  until queue.empty?
+    first = queue.shift
 
+    first[0].links.each do |node|
 
-# def sum(n, arr)
-#   if n == 1
-#     return arr << n
-#   end
+      if node.name == 'start'
+        next
+      end
 
-#   return "#{arr << n}, #{sum(n - 1, arr)}"
-# end
+      routes << [node, "#{first[1]}, #{node.name}" ]
 
+      # end node should not be added to the queue - not allowed to go back up
+      if node.name == 'end'
+        next
+      end
 
+      # small, single caves should not be added if visited
+      if node.single? && routes.last[1].scan(node.name).size > 1
+        next
+      end
 
+      # small caves should not be added if visited twice
+      if node.name == node.name.downcase && routes.last[1].scan(node.name).size > 2
+        next
+      end
 
-# get the start node
-# start a route as an empty string
-# add the start node's name
-# get the children
-# for each of them
-# is the node the end node?
-  # if it is, add it to the solution string
-  # add the node to the queue
+      queue << [node, "#{first[1]}, #{node.name} " ]
+    end
+  end
+
+  return routes
+end
+
+# nodes = NodeParser.new.generate(puzzle_input).nodes
+# binding.pry
+# start = nodes.find { |nd| nd.name == 'start' }
+
+# routes = get_routes( start, [])
+
+# p routes.select { |route| route[1].include?('end') }.size
---
+++ 12/spec/route_spec.rb
@@ -0,0 +1,156 @@
+
+require_relative '../route.rb'
+require 'rspec'
+
+
+describe '#get_routes' do
+  context 'with first set of nodes' do
+    it 'returns 10 possible routes that lead to the end' do
+      list = ["start-A",
+              "start-b",
+              "A-c",
+              "A-b",
+              "b-d",
+              "A-end",
+              "b-end"]
+
+    node_list = NodeParser.new.generate(list).nodes
+    start_node = node_list.find { |nd| nd.name == 'start' }
+
+    all_routes = get_routes(start_node, [])
+    end_routes = all_routes.select { |route| route[1].include?('end') }
+    expect(end_routes.length).to eq 10
+    end
+  end
+
+  context 'with second set of nodes' do
+    it 'returns 19 possible routes that lead to the end' do
+      list = ['dc-end',
+              'HN-start',
+              'start-kj',
+              'dc-start',
+              'dc-HN',
+              'LN-dc',
+              'HN-end',
+              'kj-sa',
+              'kj-HN',
+              'kj-dc']
+
+      node_list = NodeParser.new.generate(list).nodes
+      start_node = node_list.find { |nd| nd.name == 'start' }
+
+      all_routes = get_routes(start_node, [])
+      end_routes = all_routes.select { |route| route[1].include?('end') }
+      expect(end_routes.length).to eq 19
+    end
+  end
+
+  context 'with third set of nodes' do
+    it 'returns 226 possible routes that lead to the end' do
+      list = ['fs-end',
+              'he-DX',
+              'fs-he',
+              'start-DX',
+              'pj-DX',
+              'end-zg',
+              'zg-sl',
+              'zg-pj',
+              'pj-he',
+              'RW-he',
+              'fs-DX',
+              'pj-RW',
+              'zg-RW',
+              'start-pj',
+              'he-WI',
+              'zg-he',
+              'pj-fs',
+              'start-RW']
+
+      node_list = NodeParser.new.generate(list).nodes
+      start_node = node_list.find { |nd| nd.name == 'start' }
+
+      all_routes = get_routes(start_node, [])
+      end_routes = all_routes.select { |route| route[1].include?('end') }
+      expect(end_routes.length).to eq 226
+    end
+  end
+
+
+end
+
+describe '#get_long_routes' do
+  context 'with first set of nodes' do
+    it 'returns 36 possible routes that lead to the end' do
+      list = ["start-A",
+              "start-b",
+              "A-c",
+              "A-b",
+              "b-d",
+              "A-end",
+              "b-end"]
+
+    node_list = NodeParser.new.generate(list).nodes
+    start_node = node_list.find { |nd| nd.name == 'start' }
+
+    all_routes = get_long_routes(start_node, [])
+    binding.pry
+    end_routes = all_routes.select { |route| route[1].include?('end') }
+
+    expect(end_routes.length).to eq 36
+    end
+  end
+
+  xcontext 'with second set of nodes' do
+    it 'returns 19 possible routes that lead to the end' do
+      list = ['dc-end',
+              'HN-start',
+              'start-kj',
+              'dc-start',
+              'dc-HN',
+              'LN-dc',
+              'HN-end',
+              'kj-sa',
+              'kj-HN',
+              'kj-dc']
+
+      node_list = NodeParser.new.generate(list).nodes
+      start_node = node_list.find { |nd| nd.name == 'start' }
+
+      all_routes = get_long_routes(start_node, [])
+      end_routes = all_routes.select { |route| route[1].include?('end') }
+      expect(end_routes.length).to eq 19
+    end
+  end
+
+  xcontext 'with third set of nodes' do
+    it 'returns 226 possible routes that lead to the end' do
+      list = ['fs-end',
+              'he-DX',
+              'fs-he',
+              'start-DX',
+              'pj-DX',
+              'end-zg',
+              'zg-sl',
+              'zg-pj',
+              'pj-he',
+              'RW-he',
+              'fs-DX',
+              'pj-RW',
+              'zg-RW',
+              'start-pj',
+              'he-WI',
+              'zg-he',
+              'pj-fs',
+              'start-RW']
+
+      node_list = NodeParser.new.generate(list).nodes
+      start_node = node_list.find { |nd| nd.name == 'start' }
+
+      all_routes = get_long_routes(start_node, [])
+      end_routes = all_routes.select { |route| route[1].include?('end') }
+      expect(end_routes.length).to eq 226
+    end
+  end
+
+
+end
