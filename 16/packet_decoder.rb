949490d40d2b0e5092f11253160df8a48228d66d
Miguel Jimenez <denkatalabeno@gmail.com>
2022-01-05 01:17:25 +0800

    not even close

--- 16/packet_decoder.rb
+++ 16/packet_decoder.rb
@@ -21,35 +21,6 @@ def hex_to_bin(hex)
   hex.split('').map { |el| key[el] }.join
 end
 
-
-def decode(hex)
-  output = {}
-  bin = hex_to_bin(hex)
-  # binding.pry
-  output[:version] = bin[0..2].to_i(2)
-  output[:type_id] = bin[3..5].to_i(2)
-
-  if output[:type_id] != 4
-
-    output[:length_id] = bin[6]
-    if output[:length_id] == '0'
-
-      binding.pry
-      # get the bytes from 7..21
-      # start parsing packages
-      # how do I know if package has finished?
-    else
-    end
-  end
-
-  output
-
-
-
-
-end
-
-
 def binary_representation(literal)
   rep = literal[6..-1]
   values = ''
@@ -63,6 +34,70 @@ def binary_representation(literal)
       break
     end
   end
-
   values
 end
+
+def unpack(data, mode:)
+  if mode == :zero
+    # get the length of the bits in the packages
+    subpackages_length = data[0..14].to_i(2)
+    subpackages = packages[15..subpackages_length - 1]
+  end
+end
+
+
+def decode(packet, length=0)
+  packet_version = packet[0..2].to_i(2)
+  packet_type = packet[3..5].to_i(2)
+  length += 6
+  packet_value = ''
+  #base case
+  if packet_type == 4
+    remaining = packet[6..-1]
+    loop do
+      if remaining[0] == '0'
+        packet_value += remaining[1..4]
+        remaining = remaining[5..-1]
+        length += 5
+        break
+      end
+    packet_value += remaining[1..4]
+    remaining = remaining[5..-1]
+    length += 5
+  end
+  packet_value = packet_value.to_i(2)
+  else
+    type_id = packet[6]
+    remaining = packet[7..-1]
+    length += 1
+    if type_id == '0'
+      total_length = remaining[0..14].to_i(2)
+      remaining = remaining[15..-1]
+      length += 15
+      sub_packet_length = 0
+      values = []
+      while sub_packet_length != total_length
+        remaining, sub_packet_length, value = decode(remaining, sub_packet_length)
+        values << value
+      end
+      length += sub_packet_length
+    elsif type_id == '1'
+      total_count = remaining[0..10].to_i
+      remaining = remaining[11..-1]
+      length += 11
+      sub_packet_length = 0
+      count = 0
+      values = []
+      while count != total_count
+        remaining, sub_packet_length, value = decode(remaining, sub_packet_length)
+        values << value
+        count += 1
+      end
+      length += sub_packet_length
+    end
+  end
+  return [remaining, length, packet_value]
+end
+
+
+p decode(hex_to_bin('8A004A801A8002F478'))
--- 16/spec/packer_decoder_spec.rb
+++ 16/spec/packer_decoder_spec.rb
@@ -2,7 +2,7 @@
 require 'rspec'
 require_relative '../packet_decoder'
 
-xdescribe "#hex_to_bin" do
+describe "#hex_to_bin" do
   context "with hexadecimal string input" do
     it "converts a hexadecimal number into binary" do
       res = hex_to_bin('D2FE28')
@@ -11,42 +11,26 @@ xdescribe "#hex_to_bin" do
   end
 end
 
-
-xdescribe "#decode" do
-  context "with an operator packet with id length 1" do
-    it 'gets the version, type id, length id and number of subpackets' do
-      result = decode('EE00D40C823060')
-      expect(result[:version]).to eq(7)
-      expect(result[:type_id]).to eq(3)
-      expect(result[:length_id]).to eq(1)
-      expect(result[:subpackets]).to eq(3)
-    end
-  end
-
-  xcontext "with a operator packet with id length 0" do
-    it 'gets the version, type id, length id and number of subpackets' do
-      result = decode('38006F45291200')
-      expect(result[:version]).to eq(1)
-      expect(result[:type_id]).to eq(6)
-      expect(result[:length_id]).to eq(0)
-      expect(result[:subpackets]).to eq(2)
-    end
-  end
-
-end
-
-
 describe "#binary_representation" do
   context "with a packet containing literal values" do
     it "returns 2021 for packet D2FE28 - 110100101111111000101000" do
       res = binary_representation("110100101111111000101000")
       expect(res).to eq('011111100101')
       expect(res.to_i(2)).to eq(2021)
-
     end
+  end
+end
 
-
+describe "#unpack" do
+  context "with a packet containing a length id of 0" do
+    it "returns two subpackets for EE00D40C823060" do
+      subpackets = '0000000000110111101000101001010010001001000000000'
+      res = unpack(subpackets, :zero)
+      expect(res).to eq(['11010001010', '0101001000100100')
+    end
   end
 
+  context "with a packet containing a length id of 1" do
+  end
 
 end
