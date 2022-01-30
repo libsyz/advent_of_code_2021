218d13594d28d1d1c2d3b0c1053807ffc0712e60
Miguel Jimenez <denkatalabeno@gmail.com>
2022-01-30 13:38:04 +0100

    managed to recurse without looping

--- 18/snailfish.rb
+++ 18/snailfish.rb
@@ -5,42 +5,17 @@ def snail_add(first, second)
 end
 
 
-def explode(arr, nest)
-  # binding.pry
+def explode(pair, nest)
   # how can I know if something has been nested 4 times?
-  arr.each_with_index do |el, idx|
-    # do I have a pair?
-    dive = nil
-    if el.is_a?(Array)
-      dive = explode(el, nest + 1)
+  return pair if pair.is_a? Integer
 
-      if !dive[:left_added] && !idx.zero?
-        binding.pry
-        arr[idx - 1] += dive[:left]
-        dive[:left_added] = true
-      end
-
-      if !dive[:right_added] && arr[idx + 1]
-        arr[idx - 1] += dive[:right]
-        dive[:right_added] = true
-      end
-
-      arr[idx] = dive[:swap_value] if (nest + 1) == 4
-
-    end
-
-    if nest == 4
-      return { swap_value: 0, left: arr[0], right: arr[1], left_added: false, right_added: false }
-    end
+  if nest == 4
+    return 0
   end
 
-  if nest.zero?
-    arr
-  end
+  return pair if pair.is_a?(Array) && pair.all? { |el| el.is_a? Integer }
 
-  return dive
+  [explode(pair[0], nest + 1), explode(pair[1], nest + 1)]
 end
 
  p explode([7,[6,[5,[4,[3,2]]]]], 0)
-
- p explode([[[[[9,8],1],2],3],4], 0)
