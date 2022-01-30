e71f99084212baae5760ec19b66e7ac820207161
Miguel Jimenez <denkatalabeno@gmail.com>
2022-01-30 14:06:54 +0100

    yay! passing new specs, recursion for the win

--- 18/snailfish.rb
+++ 18/snailfish.rb
@@ -7,15 +7,24 @@ end
 
 def explode(pair, nest)
   # how can I know if something has been nested 4 times?
+
+  # binding.pry
   return pair if pair.is_a? Integer
+
+  if nest == 3
+    if pair[1].is_a? Array
+      return [pair[0] + pair[1][0], explode(pair[1], nest + 1)]
+    end
+
+    if pair[0].is_a? Array
+      return [explode(pair[0], nest + 1), pair[1] + pair[0][1] ]
+    end
+  end
 
   if nest == 4
     return 0
   end
 
-  return pair if pair.is_a?(Array) && pair.all? { |el| el.is_a? Integer }
 
   [explode(pair[0], nest + 1), explode(pair[1], nest + 1)]
 end
-
- p explode([7,[6,[5,[4,[3,2]]]]], 0)
--- 18/spec/snailfish_spec.rb
+++ 18/spec/snailfish_spec.rb
@@ -15,10 +15,10 @@ describe '#snail_add' do
 end
 
 describe "#explode" do
-  xcontext 'with an explodable element at the beginning of the array' do
+  context 'with an explodable element at the beginning of the array' do
     let(:arr) { [[[[[9,8],1],2],3],4] }
-    xit 'explodes properly' do
-      res = explode(arr)
+    it 'explodes properly' do
+      res = explode(arr, 0)
       expect(res).to eq([[[[0,9],2],3],4])
     end
   end
@@ -26,7 +26,7 @@ describe "#explode" do
   context 'with an explodable element at the end of the array' do
     let(:arr) { [7,[6,[5,[4,[3,2]]]]] }
     it 'explodes properly' do
-      res = explode(arr)
+      res = explode(arr, 0)
       expect(res).to eq([7,[6,[5,[7,0]]]])
     end
   end
