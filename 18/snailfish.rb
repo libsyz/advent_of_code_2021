07a28e23b1438415aa85243225b6d5bd48851ffb
Miguel Jimenez <denkatalabeno@gmail.com>
2022-01-30 05:37:07 +0100

    passing the first spec for a hardcoded explode

--- 18/snailfish.rb
+++ 18/snailfish.rb
@@ -1,4 +1,4 @@
-
+require 'pry-byebug'
 
 def snail_add(first, second)
   [first, second]
@@ -8,20 +8,23 @@ end
 def explode(arr)
   # how can I know if something has been nested 4 times?
   arr.each_with_index do |el, idx|
-
-    nest = 0
-    el = nil
-
+    nesting = 0
     until el.class == Integer
       el ? ( el = el[0] ) : ( el = arr[0] )
-      nest += 1
+      nesting += 1
     end
 
-    p nest
-    if nest == 3
+    if nesting == 4
+      # binding.pry
       p "explodable found at idx #{idx}"
+      left, right = arr[idx][0][0][0]
+      arr[idx][0][0][0] = 0
+      arr[idx][0][0][1] += right
     end
-
   end
 
+  arr
 end
+
+p explode([[[[[9,8],1],2],3],4])
+#=> [[[[0,9],2],3],4]
